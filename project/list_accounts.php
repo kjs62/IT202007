65e977f8f60551ae4ef308620068423c49338a73
kjs62 <kjs62@afsconnect1.njit.edu>
2020-11-15 23:20:19 -0500

    Created list_accounts with 5 limit

---
+++ project/list_accounts.php
@@ -0,0 +1,58 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<div class="drift">
+<?php
+$query = "";
+$results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT id, account_number, user_id, account_type, balance from Accounts WHERE user_id like :q LIMIT 5");
+    $r = $stmt->execute([":q" => "%$query%"]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results");
+    }
+}
+?>
+<h3>List Accounts</h3>
+<form method="POST">
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Account Number:</div>
+                        <div><?php safer_echo($r["account_number"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Account Type:</div>
+                        <div><?php safer_echo($r["account_type"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Balance:</div>
+                        <div><?php safer_echo($r["balance"]); ?></div>
+                    </div>
+                    <br>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
+</div>
