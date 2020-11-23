ef866dce0ad08360e8375dea8a36436e828b0625
kjs62 <kjs62@afsconnect1.njit.edu>
2020-11-22 19:09:52 -0500

    added respective link minus transfer

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -17,9 +17,9 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="create_checking_account.php">Create Checking Account</a></li>
-        <li><a href="#">List Accounts</a></li>
-        <li><a href="#">Deposit</a></li>
-        <li><a href="#">Withdraw</a></li>
+        <li><a href="list_accounts.php">List Accounts</a></li>
+        <li><a href="deposit.php">Deposit</a></li>
+        <li><a href="withdraw.php">Withdraw</a></li>
         <li><a href="#">Transfer</a></li>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
