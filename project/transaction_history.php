d7c31aa1929ad8c6f7b8a08c54f4a9c8c45512cb
kjs62 <kjs62@afsconnect1.njit.edu>
2020-11-22 19:04:23 -0500

    shows results for specific accounts

--- project/transaction_history.php
+++ project/transaction_history.php
@@ -1,26 +1,28 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <div class="drift">
 <?php
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+?>
+<?php
 $query = "";
 $results = [];
-if (isset($_POST["query"])) {
-    $query = $_POST["query"];
-}
+if (isset($id)) {
     $db = getDB();
     $UserId = get_user_id();
-    $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id = $UserId like :q LIMIT 10");
-    $r = $stmt->execute([":q" => "%$query%"]);
+    $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$id%"]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    else {
-        flash("There was a problem fetching the results " . var_export($stmt->errorInfo(), true));
-    }
-    $stmt2 = $db->prepare("SELECT id, account_number, account_type from Accounts WHERE id like :q");
+    
+    $stmt2 = $db->prepare("SELECT id, account_number, account_type from Accounts WHERE user_id = $UserId");
   $r2 = $stmt2->execute([":q" => "%$query%"]);
   if ($r2) {
         $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
     }
+}
 ?>
 <h3>List Transactions</h3>
 <div class="results">
