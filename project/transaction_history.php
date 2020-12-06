d5dc117ba67d9359207187e424ca566480e211c8
kjs62 <kjs62@afsconnect1.njit.edu>
2020-12-05 21:22:00 -0500

    added pagination, no filters

--- project/transaction_history.php
+++ project/transaction_history.php
@@ -11,8 +11,32 @@ $results = [];
 if (isset($id)) {
     $db = getDB();
     $UserId = get_user_id();
-    $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id like :q ORDER BY id DESC LIMIT 10");
+    
+    $page = 1;
+    $per_page = 10;
+    if(isset($_GET["page"])){
+        try {
+            $page = (int)$_GET["page"];
+        }
+        catch(Exception $e){
+        }
+    }
+    
+    $stmt = $db->prepare("SELECT count(*) as total from Transactions WHERE act_src_id like :q ORDER BY id DESC LIMIT 10");
     $r = $stmt->execute([":q" => "%$id%"]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $total = 0;
+    if($result){
+        $total = (int)$result["total"];
+    }
+    $total_pages = ceil($total / $per_page);
+    $offset = ($page-1) * $per_page;
+    
+    $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id like :q ORDER BY id DESC LIMIT :offset, :count");
+    $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
+    $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
+    $stmt->bindValue(":q", $id);
+    $r = $stmt->execute();
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
@@ -58,4 +82,17 @@ if (isset($id)) {
         <p>No results</p>
     <?php endif; ?>
 </div>
+  <nav aria-label="My Eggs">
+    <ul class="pagination justify-content-center">
+      <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+        <a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+      </li>
+      <?php for($i = 0; $i < $total_pages; $i++):?>
+      <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+      <?php endfor; ?>
+      <li class="page-item <?php echo ($page+1) >= $total_pages?"disabled":"";?>">
+        <a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo $page+1;?>">Next</a>
+      </li>
+    </ul>
+  </nav>
 </div>
